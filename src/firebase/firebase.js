4701f93f4ae874c449e5d9250e4dbc05101ac51a
Liqian Ma <liqianma2023@u.northwestern.edu>
2021-05-27 18:12:07 -0500

    renew firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
 
+
+
 const config = {
-    apiKey: "AIzaSyAqHgdA__tVb5g9CLsm6MHL602Xo0BiSbA",
-    authDomain: "account-book-8971f.firebaseapp.com",
-    databaseURL: "https://account-book-8971f-default-rtdb.firebaseio.com",
-    projectId: "account-book-8971f",
-    storageBucket: "account-book-8971f.appspot.com",
-    messagingSenderId: "563388230836",
-    appId: "1:563388230836:web:aa144329e1512962fb7be1",
-    measurementId: "G-EG7BRVZL8X"
+    apiKey:FIREBASE_API_KEY,
+    authDomain:FIREBASE_AUTH_DOMAIN,
+    databaseURL: FIREBASE_DATABASE_URL,
+    projectId: FIREBASE_PROJECT_ID ,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId:FIREBASE_MESSAGING_SENDER_ID,
+   
   };
 
 
